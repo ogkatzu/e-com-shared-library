59b0753ef126de544c5f38a4c4269b68d3361313
skatz <saarkatz0913@gmail.com>
2025-10-19 20:41:27 +0300

    Adding Dockerfiles for agents and adding python build

---
+++ vars/buildPythonService.groovy
@@ -0,0 +1,14 @@
+// vars/buildPythonService.groovy
+
+def call(String serviceDir)
+    dir(serviceDir) {
+        stage("Install Dependencies") {
+            sh 'pipenv install'
+        }
+        stage("Test Runtime") {
+            sh """
+            pipenv shell
+            python app.py 
+            """
+        }
+    }
