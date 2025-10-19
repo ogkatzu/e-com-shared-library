b5dddd53fea89b014ef5d32d8f03b01ec7ba1589
skatz <saarkatz0913@gmail.com>
2025-10-19 15:17:50 +0300

    Init commit

---
+++ vars/buildNodeService.groovy
@@ -0,0 +1,17 @@
+// vars/buildNodeService.groovy
+
+def call(Map config) {
+    stage("Install Dependencies") {
+        sh 'npm ci'
+    }
+    stage("Lint") {
+        sh 'npm run lint || true'
+    }
+    stage("Build") {
+        def packageJson = readJson file: 'package.json'
+        if (packageJson.scripts?.build) {
+            sh 'npm run build'
+        }
+    }
+    
+}
