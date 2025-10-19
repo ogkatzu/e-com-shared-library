30b1c601736a1866383775300b15fcb21e3b0732
skatz <saarkatz0913@gmail.com>
2025-10-19 20:48:33 +0300

    Adding Dockerfiles for agents and adding python build

--- vars/buildPythonService.groovy
+++ vars/buildPythonService.groovy
@@ -2,6 +2,9 @@
 
 def call(String serviceDir) {
     dir(serviceDir) {
+        stage("Verify Python") {
+            sh 'python3 --version && pip3 --version && pipenv --version'
+        }
         stage("Install Dependencies") {
             sh 'echo "Installing Dependencies"'
         }
